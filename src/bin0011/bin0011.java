0197ac8483d64b4c1affa77c2863872ee86aa6a0
1237 <92003875+12three7@users.noreply.github.com>
2022-02-12 15:24:11 +0800

    Update and rename src/abyss/ProjectAbyss.java to src/bin0011/bin0011.java

---
+++ src/bin0011/bin0011.java
@@ -0,0 +1,24 @@
+package bin0011;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+import bin0011.content.*;
+
+public class bin0011 extends Mod{
+	public bin0011(){}
+	
+	@Override
+	public void loadContent(){
+		new Bin0011Items().load();
+		new Bin0011Bullets().load();
+		new Bin0011Blocks().load();
+		new Bin0011TechTree().load();
+	}
+}
