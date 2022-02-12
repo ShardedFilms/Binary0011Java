d858d6c3cfbec5d94b0ca4e93b9762398d3b4aa9
1237 <92003875+12three7@users.noreply.github.com>
2022-02-12 19:49:18 +0800

    Update Bin0011Items.java

--- src/bin0011/content/Bin0011Items.java
+++ src/bin0011/content/Bin0011Items.java
@@ -12,16 +12,20 @@ public class Bin0011Items implements ContentList {
 	@Override
 	public void load() {
 		item00 = new Item("item00", Color.valueOf("4A4B53")) {{
+			localizedName = "item00";
 			cost = 0;
 		}};
 
 		item01 = new Item("item01", Color.valueOf("4A4B53")) {{
+			localizedName = "item01"
 			cost = 0;
 		}};
 		item10 = new Item("item10", Color.valueOf("B0BAC0")) {{
+			localizedName = "item10"
 			cost = 0;
 		}};
 		item11 = new Item("item11", Color.valueOf("B0BAC0")) {{
+			localizedName = "item11"
 			cost = 0;
 			hardness = 1;
 		}};
