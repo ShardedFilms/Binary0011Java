ff66f515bdb85f72830bef9b02b224d6b69ab585
1237 <92003875+12three7@users.noreply.github.com>
2022-02-18 22:40:48 +0800

    Update Bin0011Blocks.java

--- src/bin0011/content/Bin0011Blocks.java
+++ src/bin0011/content/Bin0011Blocks.java
@@ -1,34 +1,27 @@
 package bin0011.content;
 
 import arc.graphics.*;
-import arc.math.*;
+import arc.graphics.g2d.*;
 import arc.struct.*;
+import arc.util.*;
 import mindustry.*;
+import mindustry.content.*;
 import mindustry.ctype.*;
-import mindustry.entities.*;
-import mindustry.entities.bullet.*;
+import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.entities.bullet.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
-import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.units.*;
 import mindustry.world.blocks.defense.*;
 import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.liquid.*;
-import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
 import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
-import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
+import mindustry.world.blocks.distribution.*;
 import mindustry.world.meta.*;
+import mindustry.world.draw.*;
 
 import bin0011.content.*;
 
