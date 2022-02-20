5aae6bb0cb975af4d0947b1ee6b8b30ebc864fbc
1237 <92003875+12three7@users.noreply.github.com>
2022-02-20 10:51:15 +0800

    FIX

--- src/bin0011/content/Bin0011SectorPresets.java
+++ src/bin0011/content/Bin0011SectorPresets.java
@@ -13,7 +13,7 @@ public class Bin0011SectorPresets implements ContentList {
     @Override
     public void load(){
         //reg displan
-        dis00 = new SectorPreset("dis00", displan, 15){{
+        dis00 = new SectorPreset("dis00", Bin0011Planets.displan, 15){{
             captureWave = 30;
             difficulty = 3;
         }};
