57516bb36d7fc25a5d8c53c31b1ee3833d86fa09
1237 <92003875+12three7@users.noreply.github.com>
2022-02-20 13:48:06 +0800

    Update Bin0011SectorPresets.java

--- src/bin0011/content/Bin0011SectorPresets.java
+++ src/bin0011/content/Bin0011SectorPresets.java
@@ -16,9 +16,9 @@ public class Bin0011SectorPresets implements ContentList {
         // gw sengaja pake base 16 biar gw bisa bikin >16 misi tanpa harus khawatir angkanya bakal sampe max value (kalo bas 16 max valuenya 16^digit -> 16^2 = 256
         dis00 = new SectorPreset("dis00", Bin0011Planets.displan, 15){{
             localizedName = "dis00";
-            captureWave = 30;
-            difficulty = 3;
-            alwaysUnlocked = true;
+            captureWave = 20;
+            difficulty = 2;
+			alwaysUnlocked = true;
         }};
         // end displan
     }
