04588174ac0da25bf14c3c4375859eb6ffbd83ee
1237 <92003875+12three7@users.noreply.github.com>
2022-02-20 10:32:01 +0800

    Update Bin0011SectorPresets.java

--- src/bin0011/content/Bin0011SectorPresets.java
+++ src/bin0011/content/Bin0011SectorPresets.java
@@ -1,9 +1,9 @@
-package mindustry.content;
+package bin0011.content;
 
 import mindustry.ctype.*;
 import mindustry.type.*;
 
-import static mindustry.content.Planets.*;
+import static bin0011.content.Bin0011Planets;
 
 public class Bin0011SectorPresets implements ContentList {
     public static SectorPreset
