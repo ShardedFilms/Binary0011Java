73401043920f013148c98f5f4c77c3bb768b3542
1237 <92003875+12three7@users.noreply.github.com>
2022-02-20 10:28:21 +0800

    Create Bin0011SectorPresets.java

---
+++ src/bin0011/content/Bin0011SectorPresets.java
@@ -0,0 +1,21 @@
+package mindustry.content;
+
+import mindustry.ctype.*;
+import mindustry.type.*;
+
+import static mindustry.content.Planets.*;
+
+public class SectorPresets implements ContentList {
+    public static SectorPreset
+    dis00;
+
+    @Override
+    public void load(){
+        //reg displan
+        dis00 = new SectorPreset("dis00", displan, 15){{
+            captureWave = 30;
+            difficulty = 3;
+        }};
+        // end displan
+    }
+}
