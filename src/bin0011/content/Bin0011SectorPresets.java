dd56502e0e83d28f90f80b1f0871758d6dfa46b0
1237 <92003875+12three7@users.noreply.github.com>
2022-02-20 11:20:31 +0800

    fix?

--- src/bin0011/content/Bin0011SectorPresets.java
+++ src/bin0011/content/Bin0011SectorPresets.java
@@ -15,8 +15,10 @@ public class Bin0011SectorPresets implements ContentList {
         //reg displan
         /* naming system untuk sector beda dari naming system block. 
         gw gak pengen nama sector digitnya banyak karena pake base 2/binary. disini gw pake hexadecimal, base 16 */
-        dis00 = new SectorPreset("dis00", Bin0011Planets.displan, 15){{
-            localizedName = "dis00";
+        daciteFields = new SectorPreset("dacite-fields", Bin0011Planets.displan, 15){{
+            localizedName = "Dacite Fields";
+            addStartingItems = true;
+            startWaveMultiplier = 2f
             captureWave = 30;
             difficulty = 3;
         }};
