fc8f0cc690281e151c42313ab9a051a384d41846
1237 <92003875+12three7@users.noreply.github.com>
2022-02-23 18:18:39 +0800

    Update Bin0011Items.java

--- src/bin0011/content/Bin0011Items.java
+++ src/bin0011/content/Bin0011Items.java
@@ -14,22 +14,25 @@ public class Bin0011Items implements ContentList {
 		item00 = new Item("item00", Color.valueOf("4A4B53")) {{
 			localizedName = "item00";
 			cost = 0;
+			hardness = 1;
 		}};
 
 		item01 = new Item("item01", Color.valueOf("4A4B53")) {{
 			localizedName = "item01";
 			cost = 0;
+			hardness = 1;
 		}};
 		item10 = new Item("item10", Color.valueOf("B0BAC0")) {{
 			localizedName = "item10";
 			cost = 0;
 			explosiveness = 0.6f;
 			charge = 0.2f;
+			hardness = 2;
 		}};
 		item11 = new Item("item11", Color.valueOf("B0BAC0")) {{
 			localizedName = "item11";
 			cost = 0;
-			hardness = 1;
+			hardness = 3;
 		}};
 	}
 }
