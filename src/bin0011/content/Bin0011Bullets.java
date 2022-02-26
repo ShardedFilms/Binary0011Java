d55e2b5d610bfc6e841b84806d5d50aee5fbabfb
1237 <92003875+12three7@users.noreply.github.com>
2022-02-26 13:11:55 +0800

    Create Bin0011Bullets.java

---
+++ src/bin0011/content/Bin0011Bullets.java
@@ -0,0 +1,32 @@
+package mindustry.content;
+
+import arc.graphics.*;
+import mindustry.ctype.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.graphics.*;
+
+public class Bullets implements ContentList{
+    public static BulletType
+
+    //artillery
+    artillery00;
+
+    @Override
+    public void load(){
+
+        artillery00 = new ArtilleryBulletType(3f, 20, "shell"){{
+            hitEffect = Fx.flakExplosion;
+            knockback = 0.8f;
+            lifetime = 80f;
+            width = height = 11f;
+            collidesTiles = false;
+            splashDamageRadius = 30f * 2f;
+            splashDamage = 45f;
+            reloadMultiplier = 1.2f;
+            ammoMultiplier = 3f;
+            homingPower = 1.8f;
+            homingRange = 50f;
+        }};
+    }
+}
